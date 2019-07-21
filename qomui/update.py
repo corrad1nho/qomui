6dd42a12d61564ea77585d52cbcff3bc50b99971
Farcrada Withman <Farcrada@gmail.com>
2019-07-21 08:56:43 +0200

    New Mullvad site resulted in crash
    Fixed the way the page is scraped.
    Program would hardcrash (lack of index checks) on retrieving server lists.

--- qomui/update.py
+++ qomui/update.py
@@ -299,10 +299,12 @@ class AddServers(QtCore.QThread):
                 page = self.session.get('https://www.mullvad.net/en/servers/', timeout=2)
                 self.log.emit(("info", "Fetching server list for Mullvad"))
                 server_page = BeautifulSoup(page.content, "lxml")
-                server_parse = server_page.find_all("div", {"class":"section-content server-table"})
+                server_parse = server_page.find_all("section", {"class": "content"})
 
-                for entry in server_parse[0].find_all('tr'):
-                    info = entry.find_all('td')
+                for entry in server_parse[0].find_all("tr"):
+                    info = entry.find_all("td")
+                    if len(info) < 1:
+                        continue
                     name = info[0].string
 
                     if info[1].string != "Country":
